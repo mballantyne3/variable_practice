bcf85b05e206c3f6dbf415cc86afd686b9e9c509
Mary Ballantyne <mballantyne777@gmail.com>
2022-06-07 17:26:27 -0400

    Inputting puts

--- variables.rb
+++ variables.rb
@@ -6,3 +6,8 @@ income = 0.01
 probability = 0.5
 my_head_hurts = true
 my_head_doesnt_hurt = false
+name = "Mary Lynn"
+name2 = "Mary L"
+puts "I'm learning to code"
+puts "I'm also very confused"
+puts income
